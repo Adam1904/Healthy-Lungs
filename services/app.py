8da975fded18e19a65e49c7616c6d6e3442b8eea
AdamPiszczek <adampiszczek1904@gmail.com>
2022-06-17 19:24:21 +0200

    air polution data

--- services/app.py
+++ services/app.py
@@ -8,7 +8,9 @@ if os.path.isfile(dotenv_file):
 api_key = os.getenv('API_KEY')
 lat = "52.219428975996706"
 lon = "21.011772669314333"
-url = "https://api.openweathermap.org/data/2.5/onecall?lat=%s&lon=%s&appid=%s&units=metric" % (lat, lon, api_key)
+# url = "https://api.openweathermap.org/data/2.5/onecall?lat=%s&lon=%s&appid=%s&units=metric" % (lat, lon, api_key) <- general weather
+url = "http://api.openweathermap.org/data/2.5/air_pollution?lat=%s&lon=%s&appid=%s&units=metric" % (lat, lon, api_key)
+# https://openweathermap.org/api/air-pollution
 
 response = requests.get(url)
 data = json.loads(response.text)
