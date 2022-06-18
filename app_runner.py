ad3799b020b00cdd70c219f4ca729d9669d8ca2c
Kacper Kilianek <01151053@pw.edu.pl>
2022-06-18 20:30:28 +0200

    HTTPS

--- app_runner.py
+++ app_runner.py
@@ -3,15 +3,17 @@ import requests, json
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home():
-    url = 'http://freegeoip.net/json/{}'.format(request.remote_addr)
-    r = requests.get(url)
-    j = json.loads(r.text)
-    city = j['city']
-
-    print(city)
+    # url = 'http://freegeoip.net/json/{}'.format(request.remote_addr)
+    # r = requests.get(url)
+    # j = json.loads(r.text)
+    # city = j['city']
+    #
+    # print(city)
     return render_template("index.html")
 
+
 if __name__ == "__main__":
-    app.run(debug=True,host='0.0.0.0', port=80)
+    app.run(ssl_context=('cert.pem', 'key.pem'),debug=True, host='0.0.0.0', port=443)
