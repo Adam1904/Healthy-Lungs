20a395289e53f8074e25901090652d1978157a7f
Adam Piszczek <adampiszczek1904@gmail.com>
2022-06-20 14:59:42 +0200

    changes in application.py

--- application.py
+++ application.py
@@ -1,18 +1,31 @@
-from flask import Flask, render_template, request
-import requests, json
+import os, json, requests, dotenv
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
+def getData(lat,lon):
+    if lat == None or lon == None:
+        lat = "52.219428975996706"
+        lon = "21.011772669314333"
+
+    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    dotenv_file = os.path.join(BASE_DIR, ".env")
+    if os.path.isfile(dotenv_file):
+        dotenv.load_dotenv(dotenv_file)
+
+    api_key = os.getenv('API_KEY')
+    # https://openweathermap.org/api/
+    # url = "https://api.openweathermap.org/data/2.5/onecall?lat=%s&lon=%s&appid=%s&units=metric" % (lat, lon, api_key) <- general weather
+    # https://openweathermap.org/api/air-pollution
+    url = "http://api.openweathermap.org/data/2.5/air_pollution?lat=%s&lon=%s&appid=%s&units=metric" % (lat, lon, api_key)
+
+    response = requests.get(url)
+    data = json.loads(response.text)
+    return(data)
 
 @app.route("/")
 def home():
-    # url = 'http://freegeoip.net/json/{}'.format(request.remote_addr)
-    # r = requests.get(url)
-    # j = json.loads(r.text)
-    # city = j['city']
-    #
-    # print(city)
     return render_template("index.html")
-
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0')
