064f7c3dac670ed934d9aef78954cc4e3bb8991e
Adam Piszczek <adampiszczek1904@gmail.com>
2022-06-22 09:38:23 +0200

    fixes in index.html and index2.html

--- application.py
+++ application.py
@@ -2,6 +2,10 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@app.route("/choose-location/")
+def choose_location():
+    return render_template("index2.html")
+
 @app.route("/")
 def home():
     return render_template("index.html")
